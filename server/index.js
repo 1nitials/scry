3532490312a1a8c57980d4823c5c1cb754e120bd
ronnie <ronadjd65@gmail.com>
2026-03-04 00:34:01 +0800

    ai integration

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
+require('dotenv').config()
+
 const express = require('express')
 const cors = require('cors')
+
+const Groq = require('groq-sdk')
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY })
 
 const app = express()
 const PORT = 3001
@@ -9,6 +14,29 @@ app.use(express.json())
 
 app.get('/api/data', (req, res) => {
   res.json({ message: 'Connected to backend!' })
+})
+
+/*
+app.post('/api/data', (req, res) => {
+  const userPrompt = req.body.prompt;
+  res.json({ message: userPrompt })
+})
+*/
+
+app.post('/api/data', async (req, res) => {
+  const userPrompt = req.body.prompt;
+
+  const completion = await groq.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content: userPrompt,
+      },
+    ],
+    model: "llama-3.3-70b-versatile",
+  });
+  
+  res.json({ message: completion.choices[0].message.content })
 })
 
 app.listen(PORT, () => {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ interface ApiData {
 
 export default function Home() {
   const [data, setData] = useState<ApiData | null>(null)
+  const [prompt, setPrompt] = useState<string>('')
 
   useEffect(() => {
     fetch('http://localhost:3001/api/data')
@@ -14,12 +15,38 @@ export default function Home() {
       .catch(console.error)
   }, [])
 
+  const sendPrompt = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+
+    fetch('http://localhost:3001/api/data', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ prompt })
+    })
+    .then(res => res.json())
+    .then(setData)
+  }
+
   return (
-    <div className="w-96 h-96 p-4 bg-gray-50">
-      <h1 className="text-2xl font-bold text-blue-600 mb-4">Scry</h1>
-      <p className="text-gray-700">Tool to improve search precision and verify information.</p>
+    <div className="w-96 h-96 p-4 bg-gray-100">
+      <h1 className="text-2xl font-bold text-blue-600 mb-2">Scry</h1>
+
+      <form>
+        <textarea 
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+        style={{ resize: 'none' }} 
+        className="bg-grey border border-black w-full mt-4 p-2
+        placeholder:text-gray-400 placeholder:italic"
+        placeholder="What do you want to find?" 
+        rows={5}></textarea>
+        <button onClick={sendPrompt} className="bg-blue-700 text-white font-bold rounded-xl px-4 py-2 mt-2">Submit</button>
+      </form>
+
       {data && (
-        <div className="mt-4 p-3 bg-white rounded shadow">
+        <div className="mt-4 p-3 bg-white rounded shadow max-h-[300px] overflow-y-scroll">
           <p className="text-sm">{data.message}</p>
         </div>
       )}
